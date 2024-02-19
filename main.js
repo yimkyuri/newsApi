4a7fda54ddb2871cb03419890e0e35e1bef31e21
kyuri <yimkyuri@naver.com>
2024-02-19 14:14:52 +0900

    final

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // const API_KEY = "03e25fa0b75541378d2a78fe07b764c3"
 const keyword = "기자"
+const page = 1
+const pageSize = 2
 let news = [];
 const getLatesNews = async () => {
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-    const url = new URL(`https://flourishing-naiad-bdffdd.netlify.app/top-headlines?page=1&pageSize=20`);
+    const url = new URL(`https://flourishing-naiad-bdffdd.netlify.app/top-headlines?page=${page}&pageSize=${pageSize}&q=${keyword}`);
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles;
     console.log("ddd", news);
-    console.log(response);
 
 }
 getLatesNews();
